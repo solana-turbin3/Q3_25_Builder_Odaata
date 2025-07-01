fe2cb79d1ef9075850f235e5d1ad5f10d71af02d
Mike Olson <mike@madtownmining.com>
2025-07-01 17:49:09 -0500

    Add metadata for SPL token.

--- solana-starter/ts/cluster1/spl_metadata.ts
+++ solana-starter/ts/cluster1/spl_metadata.ts
@@ -1,16 +1,18 @@
-import wallet from "../turbin3-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
+import {bs58} from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import {createUmi} from "@metaplex-foundation/umi-bundle-defaults"
+import {
+    createMetadataAccountV3,
+    CreateMetadataAccountV3InstructionAccounts,
     CreateMetadataAccountV3InstructionArgs,
     DataV2Args
 } from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
-import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import {createSignerFromKeypair, signerIdentity, publicKey} from "@metaplex-foundation/umi";
+
+import wallet from "../turbin3-wallet.json"
+import {getTxExplorerUrl} from "./lib/getExplorerLinks";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("AYhWHaye5rdFqiAtD7AuVXqQTQD5gH8qCxAYLetf42VB")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -21,29 +23,44 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint,
+            mintAuthority: signer,
+        };
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: 'Giraffe',
+            symbol: 'GRF',
+            uri: '',
+            sellerFeeBasisPoints: 0,
+            creators: null,
+            collection: null,
+            uses: null,
+        };
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data,
+            isMutable: true,
+            collectionDetails: null,
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
-    } catch(e) {
+        let result = await tx.sendAndConfirm(umi);
+        const txBs58 = bs58.encode(result.signature)
+        console.log(txBs58);
+        console.log(getTxExplorerUrl(txBs58));
+        /**
+         * 2w4br7xvKwUq1P2b63ZMcHW1nAu78EKEL8LYmqgMRwPL5mSeNqEN8oEKg5H6YUxbcEgxqvy2mYQKSCc6dXGNtruu
+         * https://explorer.solana.com/tx/2w4br7xvKwUq1P2b63ZMcHW1nAu78EKEL8LYmqgMRwPL5mSeNqEN8oEKg5H6YUxbcEgxqvy2mYQKSCc6dXGNtruu?cluster=devnet
+         */
+    } catch (e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
 })();
