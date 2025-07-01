24840b8f015c15f184741d976f9ec4012270c228
Mike Olson <mike@madtownmining.com>
2025-07-01 16:49:27 -0500

    Init SPL token and add helpers for explorer links.

---
+++ solana-starter/ts/cluster1/lib/getExplorerLinks.ts
@@ -0,0 +1,13 @@
+import {PublicKey} from "@solana/web3.js";
+
+type ClusterName = 'devnet' | 'localnet' | 'mainnet';
+
+const EXPLORER_HOST = 'https://explorer.solana.com';
+
+export const getTxExplorerUrl = (signature: string | PublicKey, cluster: ClusterName = 'devnet') => {
+    return `${EXPLORER_HOST}/tx/${signature.toString()}?cluster=${cluster}`;
+};
+
+export const getAcctExplorerUrl = (address: string | PublicKey, cluster: ClusterName = 'devnet') => {
+    return `${EXPLORER_HOST}/address/${address.toString()}?cluster=${cluster}`;
+};
--- solana-starter/ts/cluster1/spl_init.ts
+++ solana-starter/ts/cluster1/spl_init.ts
@@ -1,6 +1,8 @@
-import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
+import {Keypair, Connection, Commitment} from "@solana/web3.js";
+import {createMint} from '@solana/spl-token';
+
 import wallet from "../turbin3-wallet.json"
+import {getAcctExplorerUrl, getTxExplorerUrl} from "./lib/getExplorerLinks";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -12,8 +14,13 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 (async () => {
     try {
         // Start here
-        // const mint = ???
-    } catch(error) {
+        const mint = await createMint(connection, keypair, keypair.publicKey, null, 6);
+        console.log(`Successfully created a mint ${mint}`);
+        console.log(`link: ${getAcctExplorerUrl(mint)}`)
+        // Successfully created a mint AYhWHaye5rdFqiAtD7AuVXqQTQD5gH8qCxAYLetf42VB
+        // https://explorer.solana.com/address/AYhWHaye5rdFqiAtD7AuVXqQTQD5gH8qCxAYLetf42VB?cluster=devnet
+        // https://explorer.solana.com/tx/4F8XTDrorD1H8sZMe6PmNXeUqfoQsySrxcbb4cVJ9jJBdgT1nazjDN23c2tqpqYQWfWT3EDt3HFgrxSDhW2FCA2K?cluster=devnet
+    } catch (error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
 })()
