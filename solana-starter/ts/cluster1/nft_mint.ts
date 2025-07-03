29329c1d5aca7c0479e84fa7f3eb651705fc9be6
Mike Olson <mike@madtownmining.com>
2025-07-02 20:35:26 -0500

    Add nft mint and mint Rug Man.

--- solana-starter/ts/cluster1/nft_mint.ts
+++ solana-starter/ts/cluster1/nft_mint.ts
@@ -1,9 +1,10 @@
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
-import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
+import {createSignerFromKeypair, signerIdentity, generateSigner, percentAmount} from "@metaplex-foundation/umi";
+import {createUmi} from "@metaplex-foundation/umi-bundle-defaults";
+import {createNft, mplTokenMetadata} from "@metaplex-foundation/mpl-token-metadata";
 
 import wallet from "../turbin3-wallet.json"
 import base58 from "bs58";
+import {getTxExplorerUrl} from "./lib/getExplorerLinks";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
 const umi = createUmi(RPC_ENDPOINT);
@@ -15,12 +16,21 @@ umi.use(mplTokenMetadata())
 
 const mint = generateSigner(umi);
 
-(async () => {
-    // let tx = ???
-    // let result = await tx.sendAndConfirm(umi);
-    // const signature = base58.encode(result.signature);
-    
-    // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+const metadataUri = 'https://gateway.irys.xyz/85ZTCm6xjV2A8o6tZqSH71pg1X7oqiwE3Xau1Fzckk5d';
 
+(async () => {
+    const result = await createNft(umi, {
+        mint,
+        sellerFeeBasisPoints: percentAmount(10),
+        name: 'Rug Man',
+        uri: metadataUri,
+    }).sendAndConfirm(umi);
+
+    const signature = base58.encode(result.signature);
+
+    console.log(`Successfully Minted! TX here:\n${getTxExplorerUrl(signature)}`);
     console.log("Mint Address: ", mint.publicKey);
-})();
+    // https://explorer.solana.com/tx/4qNCk6CZRaJE3LPzqH9tZkiXXtptDiCEcXQSKEHcSZfCYXTgF7i6iq89tH3nLYyxy1Q9yPNoStPN6eRCMLbeLxm6?cluster=devnet
+    // Mint Address:  7GJvmNtgH3Fpccd2gHdLWqCbS2HiP7MZA2mFN3at38XD
+    // NFT: https://explorer.solana.com/address/7GJvmNtgH3Fpccd2gHdLWqCbS2HiP7MZA2mFN3at38XD/metadata?cluster=devnet
+})().then(() => process.exit(0));
